5402a6a414692c62c4d1a57d1394106cc8a4d1cd
Baked Cheese <dev.adityasus@gmail.com>
2022-08-04 20:48:20 +0530

    Get player selection

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const rps = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   return rps[Math.floor(Math.random() * rps.length)];
 }
+
+function getPlayerChoice() {
+  let choice = prompt("Type Rock,Paper or Scissors");
+  choice = choice.toLowerCase();
+  return choice;
+}
+
+const computerselection = getComputerChoice();
+const playerselection = getPlayerChoice();
