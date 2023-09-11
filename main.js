f33dc4ab088f69479e4814b55ace59056e36f398
Aditya <dev.adityasus@gmail.com>
2023-09-11 19:24:11 +0530

    Adds buttons to play instead of using prompts

--- main.js
+++ main.js
@@ -5,12 +5,6 @@ function getComputerChoice() {
   return rps[Math.floor(Math.random() * rps.length)];
 }
 
-function getPlayerChoice() {
-  let choice = prompt("Type Rock,Paper or Scissors");
-  choice = choice.toLowerCase();
-  return choice;
-}
-
 function checkWinner(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return "tie";
@@ -25,33 +19,28 @@ function checkWinner(playerSelection, computerSelection) {
   }
 }
 
-function playRound() { 
+function playRound(playerselection){
   const computerselection = getComputerChoice();
-  const playerselection = getPlayerChoice();
-  console.log("Player Choice: ",playerselection);
-  console.log("Computer Choice: ",computerselection);
   const winner = checkWinner(playerselection, computerselection);
-  console.log("Outcome of The Round: ",winner);
-  winners.push(winner);
+  // winners.push(winner);
+  console.log(playerselection);
+  console.log(computerselection);
+  console.log(winner);
 }
 
 function game() {
-  for (let i = 1; i <= 5; i++) {
-    playRound();
-    console.log("-----------------------------------")
-  }
-  logWinners();
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => { 
+    button.addEventListener('click',function (){
+      playRound(this.textContent.toLowerCase());
+    });
+  });
 }
 
 function logWinners() {
   let playerWins = winners.filter((item) => item == "Player");
   let computerWins = winners.filter((item) => item == "Computer");
   let ties = winners.filter((item) => item == "tie");
-  console.log("------------------------------------");
-  console.log("Results:");
-  console.log("Player Wins: ", playerWins.length);
-  console.log("Computer Wins: ", computerWins.length);
-  console.log("Ties: ", ties.length);
 }
 
 game();
