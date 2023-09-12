2496956f7d10b61b35854b49043a1daf98ed249e
Aditya <dev.adityasus@gmail.com>
2023-09-12 22:54:07 +0530

    Displays Player & Computer choice and round winners and Game winner

--- main.js
+++ main.js
@@ -20,12 +20,21 @@ function checkWinner(playerSelection, computerSelection) {
 }
 
 function playRound(playerselection){
+  let wins = checkWins();
+  if(wins>=5){
+    return;
+  }
   const computerselection = getComputerChoice();
   const winner = checkWinner(playerselection, computerselection);
-  // winners.push(winner);
-  console.log(playerselection);
-  console.log(computerselection);
-  console.log(winner);
+  winners.push(winner);
+  document.querySelector('.player-selection').textContent = `Player: ${playerselection}`;
+  document.querySelector('.computer-selection').textContent = `Computer: ${computerselection}`; 
+  displayWins();
+
+  wins = checkWins();
+  if(wins == 5){
+    displayWinner();
+  }
 }
 
 function game() {
@@ -41,6 +50,33 @@ function logWinners() {
   let playerWins = winners.filter((item) => item == "Player");
   let computerWins = winners.filter((item) => item == "Computer");
   let ties = winners.filter((item) => item == "tie");
+}
+
+function checkWins(){
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+
+  return Math.max(playerWins,computerWins);
+}
+
+function displayWins(){
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == "tie").length;
+
+  document.querySelector('.player-wins').textContent = `Player Won: ${playerWins} times`;
+  document.querySelector('.computer-wins').textContent = `Computer Won: ${computerWins} times`;
+  document.querySelector('.ties').textContent = `Ties: ${ties} times`;
+}
+
+function displayWinner(){
+  let playerWins = winners.filter((item) => item == "Player").length;
+
+  if(playerWins == 5){
+    document.querySelector('.winner').textContent = "You won 5 times!!"
+  }else{
+    document.querySelector('.winner').textContent = "Computer won 5 times!!"
+  }
 }
 
 game();
